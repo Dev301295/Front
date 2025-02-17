61eddb1030bb73782b7cc197eef6a6eefe960d0f
Dev301295 <devanshi.vora@my-digital-school.org>
2025-02-17 12:04:18 +0100

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,8 @@ router.get('/users', (req, res) => {
   res.json([{ id: 1, name: 'Tom' }, { id: 2, name: 'Jerry' }]);
 });
 
+router.get('/users1', (req, res) => {
+  res.json([{ id: 1, name: 'Bob' }, { id: 2, name: 'Zen' }]);
+});
+
 module.exports = router;
